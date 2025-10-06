b8f030d20874e6a8164b6dc42b97c5ccfd4cd007
Ronivaldo Domingues de Andrade <ronidomingues.ard@gmail.com>
2025-10-05 22:21:54 -0300

    :sparkles: feat(script): Update script.

--- scripts/generate_presence_list.py
+++ scripts/generate_presence_list.py
@@ -6,7 +6,7 @@ BASE_PATH = os.path.dirname(os.path.abspath(__file__)) # Determinando como base 
 FILES_PATH = os.path.join(BASE_PATH, "..", "presences") # Caminho para a pasta "../presences";
 OUTPUT_FILE = os.path.join(BASE_PATH, "..", "materials", "presence_list.tex") # Caminho para o arquivo de saída "../materials/presence_list.tex";
 
-# Coletando todos os arquivos .txt no caminho fornecido
+# Coletando todos os arquivos .txt no caminho fornecido;
 files = [file for file in os.listdir(FILES_PATH) if file.endswith('.txt')]
 
 # Gerando o conteúdo do arquivo LaTeX "presence_list.tex";
@@ -19,13 +19,17 @@ content.append("\\hline")
 for name in sorted(files):
     path = os.path.join(FILES_PATH, name)
     person_name = os.path.splitext(name)[0]
-    # Obtendo a data de criação do arquivo
+
+    # Obtendo a data de criação do arquivo;
     timestamp = os.path.getctime(path)
     create_date = datetime.fromtimestamp(timestamp).strftime('%d/%m/%Y ás %H:%M:%S')
     content.append(f"   {person_name} & {create_date} \\\\")
     content.append("\\hline")
 content.append("\\end{tabularx}")
 
-# Escrevendo o conteúdo no arquivo de saída
+# Garantindo que a pasta materials exista;
+os.makedirs(os.path.dirname(OUTPUT_FILE), exist_ok=True)
+
+# Escrevendo o conteúdo no arquivo de saída;
 with open(OUTPUT_FILE, 'w', encoding='utf-8') as file:
     file.write('\n'.join(content))
